d248f85d741b3750e727a020b2dab7d2c0e10c2b
marcos eduardo <165714407+MEduardoCarvalho@users.noreply.github.com>
2026-04-23 07:30:51 -0300

    Initialize Spring Boot application structure

---
+++ src/main/java/com/chacara/ChacaraApplication.java
@@ -0,0 +1,11 @@
+package com.chacara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChacaraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChacaraApplication.class, args);
+    }
+}
